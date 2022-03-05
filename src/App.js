563dadcc77094a7927e0db676ed2a8e42599d963
Timothy Chandler <ernaldis@protonmail.com>
2022-03-05 14:24:08 -0600

    add 404 page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 const Home = lazy(() => import('./pages/Home/Home'));
+const NotFound = lazy(() => import('./pages/NotFound/NotFound'));
 
 const NavBar = lazy(() => import('./components/Navbar/Navbar'));
 
@@ -16,6 +17,9 @@ const App = () => {
           <NavBar />
           <Routes>
             <Route path='/' element={<Home />} />
+          </Routes>
+          <Routes>
+            <Route path='*' element={<NotFound />} />
           </Routes>
         </Suspense>
       </Router>
---
+++ src/pages/NotFound/NotFound.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { MetaData } from '../../components/Meta/MetaData';
+import './NotFound.css';
+
+const meta = {
+  title: '404',
+  desc: 'The page your looking for is not here.',
+  url: 'https://scoutcpr.org/404',
+  img: '../../assets/C911_Pick3.JPG',
+};
+
+const NotFound = () => (
+  <div>
+    <MetaData {...meta}>
+      <meta name='prerender-status-code' content='404'></meta>
+    </MetaData>
+
+    <div className='fourOfour'>
+      <h1>404</h1>
+    </div>
+    <div className='return'>
+      <p>Page Not Found</p>
+    </div>
+  </div>
+);
+
+export default NotFound;
